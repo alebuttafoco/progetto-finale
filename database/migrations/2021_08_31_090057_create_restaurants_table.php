5861158a3767ec35b48f8f33021d5a7a00d6bf8d
alebuttafoco <buttafoco.alessandro@gmail.com>
2021-08-31 11:41:04 +0200

    creata e settata migrazione

---
+++ database/migrations/2021_08_31_090057_create_restaurants_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRestaurantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('restaurants', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('user_id');
+            $table->string('name');
+            $table->string('image')->nullable();
+            $table->string('address');
+            $table->smallInteger('cap'); //5 cifre
+            $table->string('city');
+            $table->text('description')->nullable();
+            $table->bigInteger('piva'); //11 cifre
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('restaurants');
+    }
+}
