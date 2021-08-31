6b077d889839f88dab03eb3aa9005596d75bf23a
alebuttafoco <buttafoco.alessandro@gmail.com>
2021-08-31 16:52:49 +0200

    aggiunto metodo per restaurants.show

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Restaurant;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
@@ -9,5 +10,10 @@ class PageController extends Controller
     public function home()
     {
         return view('home');
+    }
+
+    public function showRestaurant(Restaurant $restaurant)
+    {
+        return view('restaurants.show', compact('restaurant'));
     }
 }
