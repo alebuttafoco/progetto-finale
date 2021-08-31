f271292532743e84e077c16ae726846449acd4d0
AndreiBurbulia <77996991+AndreiBurbulia@users.noreply.github.com>
2021-08-31 15:20:05 +0200

    Add many to many (plate_order)

--- app/Order.php
+++ app/Order.php
@@ -3,8 +3,20 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Order extends Model
 {
-    protected $fillable = ['customer_name', 'customer_lastname', 'customer_address', 'status', 'date', 'total_price'];
+    protected $fillable = [
+        'customer_name',
+        'customer_lastname',
+        'customer_address',
+        'status', 'date',
+        'total_price'
+        ];
+
+    public function plates() :BelongsToMany
+    {
+        return $this->belongsToMany(Plate::class);
+    }
 }
