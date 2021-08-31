12f2448734579edd6f8a0cc6afd924b072181efb
fdrbrs <federio.borsci@gmail.com>
2021-08-31 18:16:30 +0200

    creata cartella per .scss restrnt, importati app.s

--- resources/views/restaurants/show.blade.php
+++ resources/views/restaurants/show.blade.php
@@ -1,5 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>RISTORANTE</h1>
+
+    <div class="container">
+        <h1>RISTORANTE</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum blanditiis eligendi explicabo sed accusantium. Ex, rerum, accusantium architecto praesentium vero dolorum laborum deleniti nemo nam neque eum adipisci corporis accusamus.</p>
+        <div class="menu">
+            <h2>menu 1</h2>
+        </div>
+        <div class="menu">
+            <h2>menu 2</h2>
+        </div>
+        <div class="menu">
+            <h2>menu 3</h2>
+        </div>
+
+    </div>
+    
 @endsection
