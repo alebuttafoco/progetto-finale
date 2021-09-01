8f41737d55d61b8b6f422ad1f8e99e61eb2c47d7
alebuttafoco <buttafoco.alessandro@gmail.com>
2021-09-01 12:53:58 +0200

    aggiunta rotta API categorie

--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::apiResource('restaurants', 'Api\RestaurantController');
+Route::apiResource('categories', 'Api\CategoryController');
