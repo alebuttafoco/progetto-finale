44ae5797697b599d0ed469f3175b1541d4f88f30
alebuttafoco <buttafoco.alessandro@gmail.com>
2021-09-01 10:21:54 +0200

    creazione restaurant controller API

---
+++ app/Http/Controllers/Api/RestaurantController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\RestaurantResource;
+use App\Restaurant;
+use Illuminate\Http\Request;
+
+class RestaurantController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return RestaurantResource::collection(Restaurant::orderBy('id', 'DESC')->with('categories', 'user')->get());
+    }
+
+}
