d574a5ca687948551cd0d631c672578df99ef34a
peppe-p <ombrachiara96@gmail.com>
2021-09-01 15:20:33 +0200

    Inizializzato show del ristorante

--- resources/views/admin/restaurant/show.blade.php
+++ resources/views/admin/restaurant/show.blade.php
@@ -5,6 +5,11 @@
 @endsection
 
 @section('content')
+    <div class="actions_bar">
+        <a href="{{ route('admin.restaurant.index') }}" class="btn btn-primary" role="button" aria-pressed="true"><i
+                class="fa fa-chevron-left" aria-hidden="true"></i> Indietro</a>
+    </div>
+
     <div>
         <p>{{ $restaurant->description }}</p>
     </div>
