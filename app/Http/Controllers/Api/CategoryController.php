70c6e25c87d61b8296a5eee6880f3f1fdd2c4390
alebuttafoco <buttafoco.alessandro@gmail.com>
2021-09-01 12:54:15 +0200

    creato controller API categorie

---
+++ app/Http/Controllers/Api/CategoryController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Category;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\CategoryResource;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return CategoryResource::collection(Category::with('restaurants')->get());
+    }
+
+}
