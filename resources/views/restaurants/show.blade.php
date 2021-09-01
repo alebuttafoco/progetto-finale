ae9ad93d888826ee331c5a70feb02426f88478d4
fdrbrs <federio.borsci@gmail.com>
2021-09-01 10:51:43 +0200

    aggiunta card per nome ristorante

--- resources/views/restaurants/show.blade.php
+++ resources/views/restaurants/show.blade.php
@@ -3,8 +3,16 @@
 @section('content')
 
     <div class="container">
-        <h1>RISTORANTE</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum blanditiis eligendi explicabo sed accusantium. Ex, rerum, accusantium architecto praesentium vero dolorum laborum deleniti nemo nam neque eum adipisci corporis accusamus.</p>
+
+        <div class="card">
+            <div class="card-body">
+                <h1>RISTORANTE: {{$restaurant->name}}</h1>
+                <p>{{$restaurant->description}}</p>
+
+            </div>
+        </div>
+        
+        
         <div class="menu">
             <h2>menu 1</h2>
         </div>
