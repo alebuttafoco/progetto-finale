c485559020bb6d4811a444169698cb567a4b7538
peppe-p <ombrachiara96@gmail.com>
2021-09-01 15:35:00 +0200

    importati metodi auth e storage

--- app/Http/Controllers/RestaurantController.php
+++ app/Http/Controllers/RestaurantController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Restaurant;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 
 class RestaurantController extends Controller
 {
