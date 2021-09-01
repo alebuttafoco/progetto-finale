97941f7e95cfa28c30cc316b8090fc6fa28e263b
10xMike <michelegec@gmail.com>
2021-09-01 18:35:29 +0200

    Aggiunte modifiche SCSS per Cart

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Order;
 use App\Restaurant;
 use Illuminate\Http\Request;
 
@@ -15,5 +15,10 @@ class PageController extends Controller
     public function showRestaurant(Restaurant $restaurant)
     {
         return view('restaurants.show', compact('restaurant'));
+    }
+
+    public function showCart(Order $order)
+    {
+        return view('cart', compact('order'));
     }
 }
