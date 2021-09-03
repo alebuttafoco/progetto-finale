02ba04f383e3fe2b9492fcfc6f22ae4d371d443e
10xMike <michelegec@gmail.com>
2021-09-03 11:41:06 +0200

    Aggiunto Componente Carrello

---
+++ resources/js/components/CartComponent.vue
@@ -0,0 +1,85 @@
+<template>
+    <div>
+        <nav>
+    <div class="container">
+        <h1 class="p-3">Carrello</h1>
+
+        <div class="shopping-cart">
+          <div class="item">
+            <div class="image">
+              <img src="#" alt=""> 
+            </div>
+
+            <div class="description">
+              <span>Carbonara di Andrei</span>
+            </div>
+
+            <div class="quantity">
+              <button class="plus-btn" type="button" name="button">
+                <i class="fas fa-plus"></i>
+              </button>
+              <input type="text" name="name" value="1">
+              <button class="minus-btn" type="button" name="button">
+                <i class="fas fa-minus"></i>
+              </button>
+            </div>
+
+          </div>
+          <hr>
+        </div>
+        
+    </div>
+</nav>
+    </div>
+</template>
+
+<script>
+import Axios from 'axios';
+
+export default {
+    data() {
+        return{
+            
+        }
+    },
+    methods: {
+        
+    },
+    mounted(){
+        
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+/* */ 
+.shopping-cart {
+    width: 46.875rem;
+    height: 25rem;
+    margin: 5rem auto;
+    background: #FFFFFF;
+    box-shadow: 1px 2px 3px 0px rgba(0,0,0,0.10);
+    border-radius: .375rem;
+   
+    display: flex;
+    flex-direction: column;
+}
+.item {
+    padding: 20px 30px;
+    height: 3.125rem;
+    display: flex;
+  }
+.buttons {
+    position: relative;
+    padding-top: 1.875rem;
+}
+.plus-btn, .minus-btn{
+    border-radius: 3px;
+    width: 23px;
+    background-color: white;
+    border: 1px solid black;
+}
+.quantity{
+    margin-left: auto;
+}
+</style>
--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -2,62 +2,6 @@
 
 @section('content')
 
-<nav>
-    <div class="container">
-        <h1 class="p-3">Carrello</h1>
-
-        <div id="border" class="card-cart">
-
-      <section>
-        <img class="image_border">
-        <h1>
-          Order Summary
-        </h1>
-      </section>
-
-      <div class="container">
-        <p>
-           
-        </p>
-      </div>
-
-
-      <div class="grid">
-        <button class="flex-container">
-          <div class="flex-item">
-            
-          </div>
-          <div class="flex-item">
-            <h3>Annual Plan</h2>
-              <span>
-                $59.99/year
-              </span>
-          </div>
-          <div class="flex-item">
-            <h3>
-              <a href="#">Change</a>
-            </h3>
-          </div>
-        </button>
-      </div>
-
-      <div>
-        <button class="lilac">
-          Proceed to Payment
-        </button>
-      </div>
-
-
-      <div>
-        <span class="dark-blue">
-          Cancel Order
-        </span>
-      </div>
-
-
-
-    </div>
-
-</nav>
+<cart-component></cart-component>
 
 @endsection
