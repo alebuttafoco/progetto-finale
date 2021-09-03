7dd92b6318b6e83643b22f3182b5efda350d4317
peppe-p <ombrachiara96@gmail.com>
2021-09-03 15:48:24 +0200

    perfezionato show admin restaurant

--- resources/views/admin/restaurant/show.blade.php
+++ resources/views/admin/restaurant/show.blade.php
@@ -6,7 +6,10 @@
 
 @section('content')
 
-
+    <div class="actions_bar mb-5">
+        <a href="{{ route('admin.restaurant.index') }}" class="btn btn-primary" role="button" aria-pressed="true"><i
+                class="fa fa-chevron-left" aria-hidden="true"></i> Indietro</a>
+    </div>
     <div class="container p-0 w-auto" id="single-restaurant-page">
         <div class="img-restaurant-container">
             <img class="img-restaurant"
@@ -31,18 +34,24 @@
             <h3 class="p-3">Menù e i tuoi piatti</h3>
 
             <div class="d-flex flex-wrap">
-                @foreach ($plates as $plate)
-                    <div class="card m-3 position-relative" style="width: 15rem;">
-                        <img class="card-img-top plate-img" src="{{ $plate->image }}" alt="{{ $plate->name }}">
-                        <div class="card-body">
-                            <h5 class="card-title">{{ $plate->name }}</h5>
-                            <p class="card-text mb-5">{{ $plate->description }}</p>
-                            <a href="#" class="btn btn-primary price-btn"><strong>{{ $plate->price }} €</strong></a>
-                        </div>
-                    </div>
-                @endforeach
+                @if (empty($plates))
 
+                    @foreach ($plates as $plate)
+                        <div class="card m-3 position-relative" style="width: 15rem;">
+                            <img class="card-img-top plate-img" src="{{ $plate->image }}" alt="{{ $plate->name }}">
+                            <div class="card-body">
+                                <h5 class="card-title">{{ $plate->name }}</h5>
+                                <p class="card-text mb-5">{{ $plate->description }}</p>
+                                <a href="#" class="btn btn-primary price-btn"><strong>{{ $plate->price }} €</strong></a>
+                            </div>
+                        </div>
+                    @endforeach
+
+                @else
+                    <h5>Nessun piatto</h5>
+                @endif
             </div>
+
         </div>
 
     </div>
