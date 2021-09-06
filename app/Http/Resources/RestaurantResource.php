44534935edb62d123dd85bdee81a72ce7342386f
alebuttafoco <buttafoco.alessandro@gmail.com>
2021-09-06 18:15:02 +0200

    settata risorsa con relazioni

--- app/Http/Resources/RestaurantResource.php
+++ app/Http/Resources/RestaurantResource.php
@@ -14,6 +14,20 @@ class RestaurantResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            "id" => $this->id,
+            "user_id"=> $this->user_id,
+            "name" => $this->name,
+            "image" => $this->image,
+            "address" => $this->address,
+            "cap" => $this->cap,
+            "city" => $this->city,
+            "description" => $this->description,
+            "piva" => $this->piva,
+
+            'categories' => CategoryResource::collection($this->categories),
+            'plates' => PlateResource::collection($this->plates),
+        ];
+        // return parent::toArray($request);
     }
 }
