41774d06b36f6da177eb99b12acb0088b000b27b
10xMike <michelegec@gmail.com>
2021-09-06 17:01:56 +0200

    Aggiunto box del Carrello

--- resources/js/components/CartComponent.vue
+++ resources/js/components/CartComponent.vue
@@ -51,10 +51,52 @@
 
           <div class="d-flex price-border">
               <span class="p-4">Totale</span>
-              <span class="full-price p-4">12,3 €</span>
+              <!-- <span class="full-price p-4">12,3 €</span> -->
           </div>
         </div>
         
+
+        <!-- box del riepilogo del carrello -->
+        <div class="cart-box">
+          <div class="cart-header">
+            <h5 class="cart-heading">Riepilogo ordine</h5>
+            <!-- <h5>Rimuovi tutto</h5> -->
+          </div>
+          <div class="cart-items">
+            <!-- <div class="image-box">
+              <img src="#" alt="">
+            </div> -->
+            <div class="description">
+              <span class="plate-name"></span>
+            </div>
+            <div class="item-counter">
+              <!-- numero dei piatti ordinati -->
+              <div class="btn">+</div>
+              <div class="count">1</div>
+              <div class="btn">-</div>
+            </div>
+            <div class="price">
+              <!-- inserisci prezzo intero // prima del coupon -->
+              <div class="amount">1.99€</div>
+              <div class="save">Salva per dopo</div>
+              <div class="remove">Rimuovi</div>
+            </div>
+          </div>
+          <hr>
+          <div class="checkout">
+            <div class="total">
+
+              <div>
+                <div class="subtotal">3.99€</div>
+                <div class="items-number">1</div>
+              </div>
+
+              <div class="full-price">3.99€</div>
+            </div> <button>Conferma e paga</button>
+          </div>
+          
+          
+        </div>
     </div>
 
 
@@ -138,4 +180,37 @@ export default {
     border-top: 1px solid black;
 }
 
+.cart-box{
+  width: 25%;
+  height: 25%;
+  background-color: #fff;
+  border-radius: 20px;
+  /* box-shadow: 0 25px 40px; */
+}
+.cart-header{
+  margin: auto;
+  width: 90%;
+  height: 10%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.cart-heading{
+  font-size: 20px;
+  font-family: 'Open Sans';
+  font-weight: 600;
+}
+.cart-items{
+  margin: auto;
+  width: 90%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.description{
+  height: 100%;
+}
+.plate-name{
+  
+}
 </style>
