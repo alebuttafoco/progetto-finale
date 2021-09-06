9118a2612ddf3bac687684df48edfa2e67e8f6de
alebuttafoco <buttafoco.alessandro@gmail.com>
2021-09-06 18:15:43 +0200

    imporato e settato vue-router

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,6 +8,9 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
+
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
@@ -18,17 +21,39 @@ window.Vue = require('vue');
 
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+import App from "./components/App.vue";
 
-Vue.component('home-component', require('./components/HomeComponent.vue').default);
+
+import HomeComponent from "./components/HomeComponent.vue";
+import RestaurantComponent from "./components/RestaurantComponent.vue";
+// Vue.component('home-component', require('./components/HomeComponent.vue').default);
+// Vue.component('restaurant-component', require('./components/RestaurantComponent.vue').default);
+
+
 Vue.component('cart-component', require('./components/CartComponent.vue').default);
-Vue.component('restaurant-component', require('./components/RestaurantComponent.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
-
+ const router = new VueRouter({
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            component: HomeComponent,
+            name: 'restaurants',
+        },
+        {
+            path: '/restaurants/:id',
+            component: RestaurantComponent,
+            name: 'restaurants.show',
+        },
+    ]
+})
 const app = new Vue({
     el: '#app',
+    components: {App},
+    router
 });
