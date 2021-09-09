177f3126cc62e0b49467861dfa80778409bf1631
alebuttafoco <buttafoco.alessandro@gmail.com>
2021-09-09 18:14:35 +0200

    implementata modale e iniziato responsive

--- resources/js/components/RestaurantComponent.vue
+++ resources/js/components/RestaurantComponent.vue
@@ -1,15 +1,10 @@
 <template>
-  <div class="wrapper py-5">
+  <div class="my_container py-5">
+
+    <!-- RIRSTORANTE -->
     <div class="restaurant bg-white p-0">
       <div class="img-restaurant-container">
-        <img
-          :src="
-            restaurant.image == null
-              ? '../img/cover_restaurant.jpg'
-              : '../storage/' + restaurant.image
-          "
-          alt=""
-        />
+        <img :src=" restaurant.image == null ? '../img/cover_restaurant.jpg' : '../storage/' + restaurant.image" alt="" />
       </div>
 
       <h1 class="p-2 pl-3">{{ restaurant.name }}</h1>
@@ -21,45 +16,59 @@
       </div>
 
       <ul class="list-inline text-secondary p-2 pl-3">
-        <li
-          class="list-inline-item"
-          v-for="category in restaurant.categories"
-          :key="category.id"
-        >
+        <li class="list-inline-item" v-for="category in restaurant.categories" :key="category.id" >
           {{ category.name }}
         </li>
       </ul>
 
+      <!-- MENU -->
       <div class="menu p-2">
-        <button @click="callRestaurants()">premi</button>
         <h3 class="p-3">Menu</h3>
-        <!-- PIATTO SINGOLO -->
-        <div class="plate d-flex flex-wrap">
-          <div
-            @click="storagePlate(plate, index)"
-            class="card m-3 position-relative"
-            style="width: 15rem"
-            v-for="(plate, index) in restaurant.plates"
-            :key="plate.id"
-          >
-            <img
-              class="plate-img"
-              :src="'../storage/' + plate.image"
-              :alt="plate.name"
-            />
 
-            <div class="card-body">
-              <h5 class="card-title">{{ plate.name }}</h5>
-              <p class="card-text mb-5">{{ plate.description }}</p>
-              <a href="#" class="btn btn-primary price-btn"
-                ><strong>{{ plate.price }} €</strong></a
-              >
-            </div>
+        <!-- PIATTO SINGOLO -->
+        <div class="plates">
+          <div  @click="selectModalPlate(plate)" class="my_card" v-for="plate in visible_plates" :key="plate.id">
+
+              <img @click="showModal = true" class="plate-img" :src="'../storage/' + plate.image" :alt="plate.name" />
+
+              <div class="card_body">
+                <div @click="showModal = true" class="details">
+                  <h5 class="card_title">{{ plate.name }}</h5>
+                  <div class="card_text">{{ plate.description }}</div>
+                  <div class="price_btn"><strong>{{ plate.price }} €</strong></div>
+                </div>
+                <div @click="storagePlate(plate)" class="add_plate w-100 btn btn-success"><strong>Aggiunti al carrello</strong></div>
+              </div>
+
           </div>
         </div>
+
       </div>
     </div>
 
+    <!-- MODALE DEL PIATTO -->
+    <div v-if="showModal" class="overlay_modal">
+
+      <div class="plate_modal">
+        <div class="top">
+            <img :src=" '../storage/' + modal_plate.image " alt="" >
+            <i @click="showModal = false" class="fas fa-times"></i>
+        </div>
+
+        <div class="bottom">
+          <div class="details">
+            <h2>{{modal_plate.name}}</h2>
+            <h3>{{modal_plate.price}} €</h3>
+            <p class="description">{{modal_plate.description}}</p>
+          </div>
+
+          <!-- aggiungi al carrello NON FUNZIONA (NON SEI IN UN CICLO) -->
+          <div @click="storagePlate(modal_plate)" class="add_plate w-100 btn btn-success"><strong>Aggiunti al carrello</strong></div>
+        </div>
+      </div>
+
+    </div>
+    <!-- CARRELLO -->
     <div class="cart">
       <div class="cart_heading btn_large">
         <i class="fas fa-shopping-cart"></i>
@@ -71,29 +80,44 @@
           <span>{{ plate.name }}</span>
 
           <div class="actions">
-            <i
-              @click="storagePlate(plate, index)"
-              class="fas fa-plus-circle text-success"
-            ></i>
+            <i @click="minusPlate(plate, index)" class="fas fa-minus-circle text-danger" ></i>
             <span>{{ plate.qty }}</span>
-            <i
-              @click="minusPlate(plate, index)"
-              class="fas fa-minus-circle text-danger"
-            ></i>
+            <i @click="storagePlate(plate, index)" class="fas fa-plus-circle text-success" ></i>
           </div>
         </div>
-        <span
-          v-if="plates.length != 0"
-          @click="emptyCart()"
-          class="empty_cart bg-danger text-white"
-          >Svuota il carrello <i class="fas fa-trash-alt"></i
-        ></span>
+        <span v-if="plates.length != 0" @click="emptyCart()" class="empty_cart bg-danger text-white">Svuota il carrello <i class="fas fa-trash-alt"></i></span>
 
-        <a class="checkout_link btn btn-success" href="../cart"
-          >Vai alla cassa</a
-        >
+        <a class="checkout_link btn btn-success" href="../cart">Vai alla cassa</a>
       </div>
     </div>
+
+    <!-- CARRELLO MOBILE -->
+    <div class="mobile_cart">
+      <i v-if="mobileCart == false" @click="showMobileCart()" class="fas fa-shopping-bag"></i>
+
+      <div v-else class="cart_modal">
+        <div class="cart_heading btn_large">
+          <span>totale {{ cart_price() }} €</span>
+          <span @click="showMobileCart()">chiudi</span>
+        </div>
+
+        <div class="content">
+            <div class="cart_item" v-for="(plate, index) in plates" :key="plate.id">
+                <span span>{{ plate.name }}</span>
+
+                <div class="actions">
+                  <i @click="minusPlate(plate, index)" class="fas fa-minus-circle text-danger" ></i>
+                  <span>{{ plate.qty }}</span>
+                  <i @click="storagePlate(plate, index)" class="fas fa-plus-circle text-success" ></i>
+                </div>
+            </div>
+          <!-- <span v-if="plates.length != 0" @click="emptyCart()" class="empty_cart bg-danger text-white">Svuota il carrello <i class="fas fa-trash-alt"></i></span> -->
+
+          <a class="checkout_link btn btn-success" href="../cart">Vai alla cassa</a>
+        </div>
+      </div>
+    </div>
+  
   </div>
 </template>
 
@@ -103,13 +127,32 @@ import Axios from "axios";
 export default {
   data() {
     return {
-      restaurant: "",
-      plates: [],
-      restaurantOrder: [],
+      restaurant: {},
+      restaurant_plates: {},
+      visible_plates: [],
+      mobileCart: false, //dati carrello
+      restaurantOrder: [], //dati carrello
+      plates: [], //dati carrello
+      modal_plate: {}, //modale del piatto singolo
+      showModal : false, //mostra modale
     };
   },
 
   methods: {
+    selectModalPlate(plate){
+      this.modal_plate = plate;
+    },
+    showMobileCart(){
+      if (this.mobileCart) {
+        this.mobileCart = false;
+        console.log('chiudi il carrello');
+        console.log(this.mobileCart);
+      } else {
+        this.mobileCart = true;
+        console.log(this.mobileCart);
+        console.log('apri il carrello');
+      }
+    },
     cart_price() {
       let totalPrice = 0;
       if (this.plates != null) {
@@ -123,7 +166,13 @@ export default {
       Axios.get("/api/restaurants?id=" + this.$route.params.id)
         .then((resp) => {
           this.restaurant = resp.data.data;
-          console.log(resp.data.data);
+          this.restaurant_plates = resp.data.data.plates;
+          // funzione per prendere i piatti visibili
+          this.restaurant_plates.forEach(food => {
+            if (food.visible == true) {
+              this.visible_plates.push(food);
+            }
+          })
         })
         .catch((e) => {
           console.error(e);
@@ -204,7 +253,6 @@ export default {
       }
       this.plates = JSON.parse(localStorage.getItem("plates"));
     },
-
     getRestaurantOrder() {
       if (localStorage.getItem("restaurant") == null) {
         this.saveRestaurantOrder();
@@ -217,11 +265,191 @@ export default {
     this.callRestaurants();
     this.getPlates();
     this.getRestaurantOrder();
+    
   },
-};
+}
 </script>
 
 <style lang="scss" scoped>
+.my_container {
+  display: flex;
+
+  @media screen and (max-width:1199.98px) {
+    display: block;
+  }
+}
+
+.restaurant {
+  width: 80%;
+
+  @media screen and (max-width:1199.98px) {
+    width: 100%;
+  }
+
+  .address {
+    font-size: 0.7rem;
+  }
+
+  .img-restaurant-container {
+    height: 300px;
+    width: 100%;
+
+    img {
+      width: 100%;
+      height: inherit;
+      object-fit: cover;
+    }
+  }
+
+  .plates {
+    display: flex;
+    flex-wrap: wrap;
+
+    .my_card {
+        border-radius: .5rem;
+        overflow: hidden;
+        width: calc(100% / 4 - 2rem);
+        max-height: 25rem;
+        margin: 1rem;
+        border: 1px solid rgb(238, 238, 238);
+        display: flex;
+        flex-direction: column;
+        transition: .3s ease-in-out;
+        
+        @media screen and (max-width: 1399.98px) {
+          width: calc(100% / 3 - 2rem);
+        }
+        @media screen and (max-width: 991.98px) {
+          width: calc(100% / 3 - 2rem);
+        }
+        @media screen and (max-width: 767.98px) {
+          width: calc(100% / 2 - 2rem);
+        }
+        @media screen and (max-width: 575.98px) {
+          width: calc(100% - 2rem);
+        }
+
+        .plate-img {
+          height: 40%;
+          width: 100%;
+          object-fit: cover;
+        }
+    
+        .card_body {
+          height: 60%;
+          padding: 1rem;
+          display: flex;
+          flex-direction: column;
+          justify-content: space-between;
+
+          .card_text {
+            overflow: hidden;
+            display: -webkit-box;
+            -webkit-line-clamp: 4;
+            -webkit-box-orient: vertical;
+          }
+
+          .price_btn {
+            text-align: end;
+          }
+        }
+
+        .add_plate {
+          transform: translateY(4rem);
+          transition: all 0.2s ease-in-out;
+        }
+
+    &:hover {
+      cursor: pointer;
+      transform: translatey(-5px);
+      box-shadow: 1px 5px 10px rgba(0, 0, 0, 0.336);
+      color: inherit;
+
+      .add_plate {
+        transform: translateY(0);
+      }
+    }
+
+    }  
+  }
+}
+
+.overlay_modal {
+  // overlay
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.466);
+  z-index: 999;
+  overflow: hidden;
+  // centrare la modale
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .plate_modal{
+    background-color: white;
+    width: 30%;
+    height: 80%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+
+    animation: show .3s ease-in-out;
+    @keyframes show {
+      from{
+        transform: scale(.1);
+        opacity: 0;
+      }
+      to{
+        transform: scale(1);
+        opacity: 1;
+      }
+    }
+
+    .bottom{
+      height: 60%;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+
+      .details {
+        padding: 2rem;
+
+        .description {
+          text-align: justify;
+        }
+      }
+    }
+    .top{
+      height: 40%;
+      background-color: blanchedalmond;
+      position: relative;
+
+      img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+      }
+
+      .fa-times {
+        position: absolute;
+        top: 1rem;
+        right: 1rem;
+        background-color: white;
+        border-radius: 50%;
+        width: 3rem;
+        height: 3rem;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
+    }
+  }
+}
+
 .cart {
   height: fit-content;
   width: 20%;
@@ -261,59 +489,72 @@ export default {
   .checkout_link {
     margin-top: auto;
   }
+
+  @media screen and (max-width:1199.98px) {
+    display: none;
+  }
+}
+
+.mobile_cart {
+  @media screen and (min-width:1199.98px) {
+    display: none;
+  }
+
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  bottom: 2rem;
+  right: 2rem;
+  background-color: white;
+  // border-radius: 50%;
+  // padding: 2rem;
+  box-shadow: 1px 5px 10px rgba(0, 0, 0, 0.418);
+  width: 70%;
+  max-width: 20rem;
+
+  .cart_modal {
+    width: 100%;
+  }
+
+  .cart_heading {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .content {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+
+      .cart_item {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 0.3rem;
+        border-bottom: 1px solid rgb(245, 245, 245);
+      }
+
+      .empty_cart {
+        cursor: pointer;
+        display: flex;
+        justify-content: space-between;
+        padding: 0.3rem;
+      }
+
+      .checkout_link {
+        margin-top: auto;
+      }
+  }
+
+  .fas {
+    font-size: 2rem;
+  }
 }
 
 .fas {
   cursor: pointer;
   font-size: 1.4rem;
-}
-
-.wrapper {
-  width: 80%;
-  margin: auto;
-  display: flex;
-}
-
-.restaurant {
-  width: 80%;
-}
-
-.address {
-  font-size: 0.7rem;
-}
-
-.img-restaurant-container {
-  height: 300px;
-  width: 100%;
-  img {
-    width: 100%;
-    height: inherit;
-    object-fit: cover;
-  }
-}
-
-.plate-img {
-  border-bottom-left-radius: 0.25rem;
-  border-bottom-right-radius: 0.25rem;
-  max-height: 10rem;
-  object-fit: cover;
-}
-
-.price-btn {
-  position: absolute;
-  bottom: 1rem;
-  right: 1rem;
-  width: 5rem;
-}
-
-.card {
-  transition: all 0.2s ease-in-out;
-}
-
-.card:hover {
-  cursor: pointer;
-  transform: translatey(-5px);
-  box-shadow: 1px 5px 10px rgba(0, 0, 0, 0.336);
-  color: inherit;
 }
 </style>
