23e6acdab611dba801e21481d2a934fd74f6e6a8
alebuttafoco <buttafoco.alessandro@gmail.com>
2021-09-14 15:39:36 +0200

    aggiunta classe active_route

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -15,22 +15,22 @@
         <div class="my_container dashboard">
             {{-- SIDE BAR --}}
             <nav class="sidebar">
-                <a class="nav-link active" aria-current="page" href="{{ route('admin.restaurant.index') }}">
+                <a class="{{Route::currentRouteName() == 'admin.restaurant.index' ? 'active_route' : ''}} nav-link active" aria-current="page" href="{{ route('admin.restaurant.index') }}">
                     <i class="fas fa-home fa-lg"></i>
                     <span>Home</span> 
                 </a>
 
                 @if ($restaurants->count() != 0)
 
-                <a class="nav-link" href="{{ route('admin.plate.index') }}">
+                <a class="{{Route::currentRouteName() == 'admin.plate.index' ? 'active_route' : ''}} nav-link" href="{{ route('admin.plate.index') }}">
                     <i class="fas fa-utensils fa-lg"></i>
                     <span>Piatti</span>
                 </a>
-                <a class="nav-link" href="{{ route('admin.ordini') }}">
+                <a class="{{Route::currentRouteName() == 'admin.ordini' ? 'active_route' : ''}} nav-link" href="{{ route('admin.ordini') }}">
                     <i class="fas fa-shopping-cart fa-lg"></i>
                     <span>Ordini</span>
                 </a>
-                <a class="nav-link" href="{{ route('admin.statistiche') }}">
+                <a class="{{Route::currentRouteName() == 'admin.statistiche' ? 'active_route' : ''}} nav-link" href="{{ route('admin.statistiche') }}">
                     <i class="fas fa-chart-line fa-lg"></i>
                     <span>Dashboard</span>
                 </a>
