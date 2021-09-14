88b04bae59f70564f38a14fb13c528b54b5d3ee1
alebuttafoco <buttafoco.alessandro@gmail.com>
2021-09-14 09:39:33 +0200

    risolto layout dashboard admin

--- resources/views/admin/restaurant/index.blade.php
+++ resources/views/admin/restaurant/index.blade.php
@@ -6,76 +6,82 @@
 
 @section('content')
 
-
-
     @if (!($restaurants === false))
-        <table class="bg-secondary table">
+        <table class="table">
             <thead>
-                <tr>
-                    <th scope="col">ID</th>
-                    <th scope="col">Nome</th>
-                    <th scope="col">Image</th>
-                    <th scope="col">Indirizzo</th>
-                    <th scope="col">P. IVA</th>
-                    <th scope="col">Azioni</th>
+                <tr class="text-center">
+                    <th>ID</th>
+                    <th>Nome</th>
+                    <th>Image</th>
+                    <th>Indirizzo</th>
+                    <th>P. IVA</th>
+                    <th>Azioni</th>
 
                 </tr>
             </thead>
             <tbody>
 
                 @foreach ($restaurants as $restaurant)
-                    <tr>
+                    <tr class="text-center">
                         <th>{{ $restaurant->id }}</th>
                         <td>{{ $restaurant->name }}</td>
                         <td><img src="{{ $restaurant->image == null ? asset('img/cover_restaurant.jpg') : asset('storage/' . $restaurant->image) }}" alt=""></td>
                         <td>{{ $restaurant->address }} <br> {{ $restaurant->city }} <br> {{ $restaurant->cap }}</td>
                         <td>{{ $restaurant->piva }}</td>
-                        <td>
-                            <a class="btn btn-success" href="{{ route('admin.restaurant.show', $restaurant->id) }}">
-                                <i class="fas fa-eye"></i>
-                            </a>
-
-                            <a class="btn btn-warning" href="{{ route('admin.restaurant.edit', $restaurant->id) }}">
-                                <i class="fas fa-edit"></i>
-                            </a>
-
-                            {{-- DELETE BUTTON WITH MODAL --}}
-                            <a class="btn btn-danger" data-toggle="modal"
-                                data-target="#deleteModalID{{ $restaurant->id }}"><i class="fas fa-trash-alt"></i></a>
-
-                            <!-- Modal -->
-                            <div class="modal fade" id="deleteModalID{{ $restaurant->id }}" tabindex="-1"
-                                role="dialog" aria-labelledby="modelTitleId" aria-hidden="true">
-                                <div class="modal-dialog modal-dialog-centered" role="document">
-                                    <div class="modal-content">
-                                        <div class="modal-header">
-                                            <h5 class="modal-title">Eliminazione - Ristorante N.{{ $restaurant->id }}
-                                            </h5>
-                                        </div>
-                                        <div class="modal-body">
-                                            <div class="container-fluid">
-                                                <strong>ID:</strong> {{ $restaurant->id }}
-                                                <br>
-                                                <strong>NOME RISTORANTE:</strong> {{ $restaurant->name }}
-                                                <br>
-                                                <br>
-                                                Sei sicuro di voler eliminare questo ristorante ed i suoi contenuti?
+                        
+                        {{-- actions --}}
+                        <td class="actions">
+                            <div class="d-flex flex-column">
+                                <a class="btn btn-success my-1" href="{{ route('admin.restaurant.show', $restaurant->id) }}">
+                                    <span>Visualizza</span>
+                                    <i class="fas fa-eye"></i>
+                                </a>
+    
+                                <a class="btn btn-warning my-1" href="{{ route('admin.restaurant.edit', $restaurant->id) }}">
+                                    <span>Modifica</span>
+                                    <i class="fas fa-edit"></i>
+                                </a>
+    
+                                {{-- DELETE BUTTON WITH MODAL --}}
+                                <a class="btn btn-danger my-1" data-toggle="modal" data-target="#deleteModalID{{ $restaurant->id }}">
+                                    <span>Elimina</span>
+                                    <i class="fas fa-trash-alt"></i>
+                                </a>
+    
+                                <!-- Modal -->
+                                <div class="modal fade" id="deleteModalID{{ $restaurant->id }}" tabindex="-1"
+                                    role="dialog" aria-labelledby="modelTitleId" aria-hidden="true">
+                                    <div class="modal-dialog modal-dialog-centered" role="document">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title">Eliminazione - Ristorante N.{{ $restaurant->id }}
+                                                </h5>
                                             </div>
-                                        </div>
-                                        <div class="modal-footer">
-                                            <button type="button" class="btn btn-secondary"
-                                                data-dismiss="modal">Annulla</button>
-                                            <form action="{{ route('admin.restaurant.destroy', $restaurant->id) }}"
-                                                method="post">
-                                                @csrf
-                                                @method('DELETE')
-                                                <button type="submit" class="btn btn-danger">Elimina</button>
-                                            </form>
+                                            <div class="modal-body">
+                                                <div class="container-fluid">
+                                                    <strong>ID:</strong> {{ $restaurant->id }}
+                                                    <br>
+                                                    <strong>NOME RISTORANTE:</strong> {{ $restaurant->name }}
+                                                    <br>
+                                                    <br>
+                                                    Sei sicuro di voler eliminare questo ristorante ed i suoi contenuti?
+                                                </div>
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-secondary"
+                                                    data-dismiss="modal">Annulla</button>
+                                                <form action="{{ route('admin.restaurant.destroy', $restaurant->id) }}"
+                                                    method="post">
+                                                    @csrf
+                                                    @method('DELETE')
+                                                    <button type="submit" class="btn btn-danger">Elimina</button>
+                                                </form>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
+                                {{-- END DELETE BUTTON WITH MODAL --}}
                             </div>
-                            {{-- END DELETE BUTTON WITH MODAL --}}
                         </td>
                     </tr>
 
@@ -98,10 +104,6 @@
                     </button>
                 </div>
     @endif
-
-
-
-
 
     </tbody>
     </table>
