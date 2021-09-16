c7ddb0438c647043fd5d4e97285774f06c8d0e7e
AndreiBurbulia <77996991+AndreiBurbulia@users.noreply.github.com>
2021-09-16 02:58:05 +0200

    modali e controllo carello vuoto

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -6,9 +6,23 @@
 
 <form method="POST" action="{{ Route('checkout.pay') }}">
   @csrf
-      <div class="text-center">
+      <div id="button-box" class="text-center">
         <button type="submit" id="submit-button" class="button button--small button--green">Conferma e Paga</button>
       </div>
 </form>
+
+
+
+<script>
+window.setInterval(function(){
+  plates = JSON.parse(localStorage.getItem("plates"));
+  if (plates.length == 0) {
+    document.getElementById("button-box").style.display = 'none';
+    window.history.back();
+  }
+}, 500);
+
+
+</script>
   
 @endsection
